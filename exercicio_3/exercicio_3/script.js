2533f59002a9554a00c8d6130892729c6e1c6f66
migue <miguelangelosilvacarvalho96@gmail.com>
2023-04-11 17:04:34 +0100

    se

--- exercicio_3/exercicio_3/script.js
+++ exercicio_3/exercicio_3/script.js
@@ -1,17 +1,19 @@
 // flickr api key: https://www.flickr.com/services/api/misc.api_keys.html || 99c1ee9202369382a53e0a650b9e9642
 // Weather api key: https://home.openweathermap.org/users/sign_in || a9e49b305a5d14cc0790dbf507251ca1
 
-function requestFlickrApi() {
-  const inputFlicrk = document.getElementById('imageSearchID')
-    const requestOptions = {
-      method: 'GET', // GET, POST, PUT, DELETE
+const inputFlicrk = document.getElementById('imageSearchID');
 
-    }
+function requestFlickrApi() {
+  
+  const requestOptions = {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+}
+
     const requestBody={
       method :'flickr.photos.search',
       api_key:'31fa894fb022e80a7ea363cb781d1692',
       extras : 'url_l',
-      per_peg: 5,
+      per_page: 5,
       text: inputFlicrk.value,
       format : 'json',
       nojsoncallback:1
@@ -28,7 +30,8 @@ function requestFlickrApi() {
 
         const photos = response.photos.photo;
         photos.forEach((photo) => {
-          divContainer.innerHTML = divContainer.innerHTML + '<img src="' + photo.url + '">'
+
+          divContainer.innerHTML = divContainer.innerHTML + '<img src="' + photo.url_l + '">'
         })
     } );
 }
