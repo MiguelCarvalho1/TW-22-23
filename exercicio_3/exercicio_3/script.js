4e4fb8305db67b02389a5c93c9d586318d9edb53
migue <miguelangelosilvacarvalho96@gmail.com>
2023-04-11 16:26:15 +0100

    script

--- exercicio_3/exercicio_3/script.js
+++ exercicio_3/exercicio_3/script.js
@@ -1,10 +1,38 @@
-// flickr api key: https://www.flickr.com/services/api/misc.api_keys.html || a5dbe90ceeb8a3561dfc760ae33be5f7
+// flickr api key: https://www.flickr.com/services/api/misc.api_keys.html || 99c1ee9202369382a53e0a650b9e9642
 // Weather api key: https://home.openweathermap.org/users/sign_in || a9e49b305a5d14cc0790dbf507251ca1
 
 function requestFlickrApi() {
-    
+    const requestOptions = {
+      method: 'GET', // GET, POST, PUT, DELETE
+
+    }
+    const requestBody={
+      method :'flickr.photos.search',
+      api_key:'5dfa044339f2b6f8847ab60c3fec5d49',
+      extras : 'url_l',
+      per_peg: 5,
+      text: 'json',
+      nojsoncallback:1
+    }
+
+    const urlParams = new URLSearchParams(requestBody).toString();
+    fetch('https://www.flickr.com/services/rest/?'+ urlParams, requestOptions)
+    .then(result => result.json())
+    .then((response)=>{
+
+        console.log(response)
+        const divContainer = document.getElementById('photosContainer');
+        divContainer.innerHTML = '';
+
+        const photos = response.photos.photo;
+        photos.forEach((photo) => {
+          divContainer.innerHTML = divContainer.innerHTML + '<img src="' + photo.url + '">'
+        })
+    } );
 }
 
+
 function requestMeteoApi() {
+    
     
 }
